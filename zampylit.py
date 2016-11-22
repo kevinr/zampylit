0a329ca012bb5b85e3d69f2a131f81d87fa98505
Kevin Riggle <kevinr@free-dissociation.com>
2016-11-21 23:00:55 -0500

    command line arguments

--- zampylit.py
+++ zampylit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys, re
+import os, sys, re, argparse
 import arrow
 
 if os.name == 'posix' and sys.version_info[0] < 3:
@@ -9,6 +9,16 @@ else:
     import subprocess
 
 def main():
+    wdir = os.path.basename(os.getcwd())
+
+    print wdir
+    parser = argparse.ArgumentParser(description='A tool for measuring progress on group writing projects')
+    parser.add_argument('--game-name', '-g', metavar='GAMENAME', default=wdir, help="the game's name (default: %(default)s)")
+    parser.add_argument('--output-file', '-o', metavar='FILE', default=(wdir+'-wc'), help="prefix of the files to output (default: %(default)s)")
+    parser.add_argument('--extensions', '-e', metavar='EXT', default='.tex,.txt', help="extensions of files whose words to count (default: %(default)s)")
+    parser.add_argument('--namefold', '-n', metavar='FILE', help="file containing name mappings")
+    args = parser.parse_args()
+
     gitlog = subprocess.check_output(['git', 'log'], universal_newlines=True)
 
     changelog_entry_re = re.compile(r'''
@@ -24,7 +34,8 @@ def main():
         date = arrow.get(e.group('date'), 'ddd MMM D HH:mm:ss YYYY Z')
         changelog_entries.append({'commit': commit, 'author': author, 'date': date})
         
-    cmd = 'find . -type f \( -name \*.tex -o -name \*.txt \) -print0 | xargs -0 wc -w | tail -1 | grep -o "[0-9]\+"'
+    exts = ' -o '.join(['-name \*%s' % (x.strip(),) for x in args.extensions.split(',')])
+    cmd = 'find . -type f \( %s \) -print0 | xargs -0 wc -w | tail -1 | grep -o "[0-9]\+"' % (exts,)
 
     running_total = 0
     running_totals_by_author = {}
